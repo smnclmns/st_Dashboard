c86cad37f6d07c165837df1d65fc98e086ff2fa2
NightLiveAppDev <david.clemens@tu-dortmund.de>
2024-09-08 23:28:22 +0200

    log all env keys

--- convert_env_to_toml.py
+++ convert_env_to_toml.py
@@ -11,6 +11,8 @@ os.makedirs('.streamlit', exist_ok=True)
 # Erstelle eine Dictionary-Struktur für die .toml-Datei
 env_keys = ["SPREADSHEET", "WORKSHEET", "TYPE", "PROJECT_ID", "PRIVATE_KEY_ID", "PRIVATE_KEY", "CLIENT_EMAIL", "CLIENT_ID", "AUTH_URI", "TOKEN_URI", "AUTH_PROVIDER_X509_CERT_URL", "CLIENT_X509_CERT_URL"]
 
+print(f"{os.environ.keys()}")
+
 for key in env_keys:
     value = os.getenv(key)
     print(f"{key} = {value}")  # Debugging: Ausgeben der geladenen Variablen
