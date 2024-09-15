4f1fb12d9dcf0e7aabd5914542af9b174d99499a
smnclmns <simon2.clemens@tu-dortmund.de>
2024-09-15 15:41:22 +0200

    Update Connection_handling.py
    Docucmentation hinzugefügt

--- custom_moduls/Connection_handling.py
+++ custom_moduls/Connection_handling.py
@@ -1,6 +1,13 @@
+# Description: This file contains the Connection_Handler class which is used to handle the connection to the google sheets
+# and the credentials of the members. It is used to cache the connection and the credentials for 1 hour.
+
+# Imported Moduls:
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 import streamlit_authenticator as stauth
+import pandas as pd
+
+# Connection_Handler class
 
 @st.cache_resource(ttl=3600) # Cache for 1 hour
 class Connection_Handler():
@@ -9,12 +16,18 @@ class Connection_Handler():
         self.members_df = self.get_members_worksheet()
         self.credentials = self.get_credentials()
 
-    def get_members_worksheet(self):
+    def get_members_worksheet(self) -> pd.DataFrame:
+        '''
+        Returns the members worksheet as a pandas DataFrame.
+        '''
         members_df = self.conn.read(worksheet="Mitglieder")
         members_df["WhatsApp-Nr."] = members_df["WhatsApp-Nr."].astype(int).astype(str)
         return members_df
     
-    def get_credentials(self):
+    def get_credentials(self) -> dict:
+        '''
+        Returns the credentials as a dictionary.
+        '''
         _insidecredentials = {}
         for username in self.members_df["Username"].tolist():
             _insidecredentials[username] = {
