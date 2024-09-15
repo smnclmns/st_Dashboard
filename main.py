37999773ef2ce501d5b421b9adcbfba6ba4c20e1
smnclmns <simon2.clemens@tu-dortmund.de>
2024-09-15 16:03:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ elif st.session_state["authentication_status"]:
     # ----- Main App Content -----
 
     # st.link_button("Google Sheet", os.getenv("SPREADSHEET"))
-    st.html(f'<a href="{os.getenv("SPREADSHEET")}" target="_blank">Google Sheet</a>')
+    st.html(f'<a href="{ch.spreadsheet_url}" target="_blank">Google Sheet</a>')
 
     # --- Calender ---
     
